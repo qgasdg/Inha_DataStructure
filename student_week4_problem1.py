fac92a0a6bbbea32419c8d826a1caf3ebcc7b291
Shaft-H <83541581+Shaft-H@users.noreply.github.com>
2025-03-30 13:39:03 +0900

    Add files via upload

--- student_week4_problem1.py
+++ student_week4_problem1.py
@@ -1,5 +1,15 @@
 def Hanoi(n, source = "A", destination = "C", auxiliary = "B"):
-    '''=======Student's code======='''
+    #base case : n=1 
+    if n == 1:
+        print(f"Move disk 1 from source {source} to destination {destination}")
+        return
+    
+    #n-1 디스크를 source에서 auxiliary로 옮기도록 재귀 호출
+    Hanoi(n-1,source,auxiliary,destination)
+    #n 디스크를 source에서 destination로 옮김
+    print(f"Move disk {n} from source {source} to destination {destination}")
+    #n-1 디스크를 auxiliary에서 destination로 옮기도록 재귀 호출
+    Hanoi(n-1,auxiliary,destination,source)
 
 def main():
     Hanoi(n = 5,source = "A", destination = "C", auxiliary = "B")
