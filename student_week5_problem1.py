d5d39d3ffeb56df4b20818c1624e93162099bcf8
YYT\yyt <dbsxor0802@naver.com>
2025-04-09 18:23:03 +0900

    chore

--- student_week5_problem1.py
+++ student_week5_problem1.py
@@ -1,5 +1,6 @@
 import array
 
+
 class Node:
     def __init__(self, value):
         self.data = value
@@ -7,34 +8,34 @@ class Node:
         self.rightChild = None
 
     def is_leaf(self):
-        return (self.leftChild == None and self.rightChild == None)
+        return self.leftChild is None and self.rightChild is None
 
 
 def boundaryTraversal(root):
     temp_boundary = root
 
     while not temp_boundary.is_leaf():
-        print(temp_boundary.data,end=' ')
+        print(temp_boundary.data, end=' ')
         temp_boundary = temp_boundary.leftChild
 
     InorderLeafTraversal(root)
 
     temp_boundary = root.rightChild
     while not temp_boundary.is_leaf():
-        print(temp_boundary.data,end=' ')
+        print(temp_boundary.data, end=' ')
         temp_boundary = temp_boundary.rightChild
 
 
 def InorderLeafTraversal(root):
     if root.is_leaf():
-        print(root.data,end=' ')
-    
-    if root.leftChild != None:
+        print(root.data, end=' ')
+
+    if root.leftChild is not None:
         InorderLeafTraversal(root.leftChild)
-    if root.rightChild != None:
+    if root.rightChild is not None:
         InorderLeafTraversal(root.rightChild)
 
-    
+
 def main():
     #        20
     #       /  \
@@ -52,41 +53,8 @@ def main():
     root.leftChild.rightChild.rightChild = Node(14)
     root.rightChild.rightChild = Node(25)
 
-    boundaryTraversal(root) # 20 8 4 10 14 25 22
+    boundaryTraversal(root)  # 20 8 4 10 14 25 22
 
-if __name__ == "__main__":
-    main()
-=======
-import array
-
-class Node:
-    def __init__(self, value):
-        '''=======Student's code======='''
-
-'''=======Student's code======='''
-
-
-def boundaryTraversal(root):
-    '''=======Student's code======='''
-    
-def main():
-    #        20
-    #       /  \
-    #      8    22
-    #     / \     \
-    #    4   12    25
-    #       /  \
-    #      10   14
-    root = Node(20)
-    root.leftChild = Node(8)
-    root.rightChild = Node(22)
-    root.leftChild.leftChild = Node(4)
-    root.leftChild.rightChild = Node(12)
-    root.leftChild.rightChild.leftChild = Node(10)
-    root.leftChild.rightChild.rightChild = Node(14)
-    root.rightChild.rightChild = Node(25)
-
-    boundaryTraversal(root) # 20 8 4 10 14 25 22
 
 if __name__ == "__main__":
     main()
--- student_week5_problem2.py
+++ student_week5_problem2.py
@@ -3,37 +3,40 @@ class Node:
         self.data = value
         self.leftChild = None
         self.rightChild = None
-    
+
     def is_leaf(self):
-        return (self.leftChild == None and self.rightChild == None)
+        return self.leftChild is None and self.rightChild is None
+
 
 max_sum = 0
+
 
 def maxPathSum(node):
     global max_sum
     if node.is_leaf():
         return node.data
-    
+
     left_max = maxPathSum(node.leftChild)
     right_max = maxPathSum(node.rightChild)
-    
-    if left_max+node.data+right_max > max_sum:
-        max_sum = left_max+node.data+right_max
-    
-    return max(left_max,right_max)+node.data
+
+    if left_max + node.data + right_max > max_sum:
+        max_sum = left_max + node.data + right_max
+
+    return max(left_max, right_max) + node.data
+
 
 def findMaxPathSum(root):
     global max_sum
     maxPathSum(root)
     return max_sum
 
+
 def main():
     #         1
     #       /   \
     #     -2     3
     #     / \   / \
     #    8  -1  4  -5
-
 
     root = Node(1)
     root.leftChild = Node(-2)
@@ -43,48 +46,10 @@ def main():
     root.rightChild.leftChild = Node(4)
     root.rightChild.rightChild = Node(-5)
 
-    print(findMaxPathSum(root))  
-    
-    
+    print(findMaxPathSum(root))
+
     # Output: 14 (1 + -2 + 3 + 4)
 
-if __name__ == "__main__":
-    main()
-=======
-class Node:
-    def __init__(self, value):
-        '''=======Student's code======='''
-
-max_sum = 0
-
-def maxPathSum(node):
-    global max_sum
-    '''=======Student's code======='''
-
-def findMaxPathSum(root):
-    global max_sum
-    '''=======Student's code======='''
-
-def main():
-    #         1
-    #       /   \
-    #     -2     3
-    #     / \   / \
-    #    8  -1  4  -5
-
-
-    root = Node(1)
-    root.leftChild = Node(-2)
-    root.rightChild = Node(3)
-    root.leftChild.leftChild = Node(8)
-    root.leftChild.rightChild = Node(-1)
-    root.rightChild.leftChild = Node(4)
-    root.rightChild.rightChild = Node(-5)
-
-    print(findMaxPathSum(root))  
-    
-    
-    # Output: 14 (1 + -2 + 3 + 4)
 
 if __name__ == "__main__":
     main()
