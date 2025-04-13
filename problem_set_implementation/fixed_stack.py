4c9d894375cd088c48d4b6abcac9d5ee5095c231
YYT\yyt <dbsxor0802@naver.com>
2025-04-13 22:45:44 +0900

    chore:

---
+++ problem_set_implementation/fixed_stack.py
@@ -0,0 +1,24 @@
+import array
+
+
+class Stack:
+    def __init__(self, n):
+        '''=======Student's code======='''
+
+    def push(self, item):
+        '''=======Student's code======='''
+
+    def pop(self):
+        '''=======Student's code======='''
+
+    def top(self):
+        '''=======Student's code======='''
+
+    def print(self):
+        '''=======Student's code======='''
+
+    def is_empty(self):
+        '''=======Student's code======='''
+
+    def is_full(self):
+        '''=======Student's code======='''
