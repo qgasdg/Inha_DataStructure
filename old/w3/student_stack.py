83ce01ef21a0e1ed23775b6a3458729eb9a98d64
YYT\yyt <dbsxor0802@naver.com>
2025-04-16 15:34:22 +0900

    feat: student_stack.py

--- old/w3/student_stack.py
+++ old/w3/student_stack.py
@@ -2,23 +2,44 @@ import array
 
 class Stack:
     def __init__(self, n): 
-        '''=======Student's code======='''
+        self.capacity = n
+        self.stack = array.array('h', [0] * n)
+        self.top = -1
     
     def push(self, item): 
-        '''=======Student's code======='''
+        if self.is_full():
+            print("Stack is full")
+            return
+        self.top += 1
+        self.stack[self.top] = item
+        return
 
     def pop(self): 
-        '''=======Student's code======='''
-
+        if self.is_empty():
+            print("Stack is empty")
+            return None
+        item = self.stack[self.top]
+        self.top -= 1
+        return item
+        
     def top(self): 
-        '''=======Student's code======='''
-
+        if self.is_empty():
+            print("Stack is empty")
+            return None
+        return self.stack[self.top]
+        
     def print(self): 
-        '''=======Student's code======='''
-
+        if self.is_empty():
+            print("Stack is empty")
+            return
+        for i in range(self.top, -1, -1):
+            print(self.stack[i], end=' ')
+        print()
+        return
+        
     def is_empty(self): 
-        '''=======Student's code======='''
+        return self.top == -1
 
     def is_full(self): 
-        '''=======Student's code======='''
-    
+        return self.top == self.capacity - 1
+
