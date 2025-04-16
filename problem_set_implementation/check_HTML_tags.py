2bcfe08bdd223c35deffdb818b714574230b2ba8
YYT\yyt <dbsxor0802@naver.com>
2025-04-16 15:34:41 +0900

    feat: check_HTML_tags.py

---
+++ problem_set_implementation/check_HTML_tags.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, data):
+        self.data = data   
+        self.next = None   
+
+class Stack:
+    def __init__(self):
+        self.size = 0      
+        self.top = None    
+
+    def push(self, item):
+        new_node = Node(item) 
+        self.size += 1        
+        if not self.top:
+            self.top = new_node
+            return
+
+        new_node.next = self.top
+        self.top = new_node
+        return
+
+    def pop(self):
+        if not self.top:
+            return None
+        ret = self.top.data 
+        self.top = self.top.next 
+        self.size -= 1         
+        return ret
+
+    def top(self):
+        return self.top
+
+    def print(self):
+        curr = self.top
+        while curr:
+            print(curr.data, end=' ')
+            curr = curr.next
+        print()
+        return
+
+    def is_empty(self):
+        return self.size == 0
+
+with open("problem_set_implementation\HTML.txt", "r") as file:
+    lines = file.readlines()
+    lines = [line.strip().split() for line in lines if line.strip()]
+    opening_tags = Stack()
+    for line in lines:
+        for tag in line:
+            if tag[0] == '<' and tag[1] != '/':
+                opening_tags.push(tag)
+            elif tag[0] == '<' and tag[1] == '/':
+                if opening_tags.is_empty():
+                    print("HTML is not well-formed")
+                    break
+                else:
+                    opening_tag = opening_tags.pop()
+                    check = True
+                    for i in range(1, len(opening_tag)):
+                        if opening_tag[i] != tag[i + 1]:
+                            print("HTML is not well-formed")
+                            check = False
+                            break
+                    if check:
+                        print(f"Matched: {opening_tag} and {tag}")
+                    
