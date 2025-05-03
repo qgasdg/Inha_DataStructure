184e18cfdc6418b30b24fa5e7a8f1d340cfbd6b6
YYT\yyt <dbsxor0802@naver.com>
2025-05-03 17:24:27 +0900

    test: add TC2

--- student_week9_problem3.py
+++ student_week9_problem3.py
@@ -81,6 +81,38 @@ def main():
     print("Merged List: ", end='')
     print_linkedList(merged_list.head)
 
+    # -- TestCase2 --
+    """
+    1 -> 2 -> 3 -> 4 -> 5 : head1 소진
+    5 -> 6 -> 6 : 나머지 head2를 전부 잘 연결할 수 있는가?
+    """
+
+    # Create two linked lists
+    # List 1: 1 -> 3 -> 5
+    head1 = Node(1)
+    head1.next = Node(3)
+    head1.next.next = Node(5)
+
+    # List 2: 2 -> 4 -> 6 -> 6
+    head2 = Node(2)
+    head2.next = Node(4)
+    head2.next.next = Node(6)
+    head2.next.next.next = Node(6)
+
+    # Print the original lists
+    print("List 1: ", end='')
+    print_linkedList(head1) # 1 3 5
+    print("List 2: ", end='')
+    print_linkedList(head2) # 2 4 6 6
+
+    # Merge the lists
+    merged_list = MergeLinkedList()
+    merged_list.addList(head1)
+    merged_list.addList(head2)
+
+    # Print the merged list
+    print("Merged List: ", end='')
+    print_linkedList(merged_list.head) # 1 2 3 4 5 6 6
 
 if __name__ == '__main__':
     main()
