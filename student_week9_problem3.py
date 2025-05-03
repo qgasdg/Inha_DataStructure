7e04a5d067fd2b5440d502ca1c705f8cf692a475
YYT\yyt <dbsxor0802@naver.com>
2025-05-03 16:21:11 +0900

    refactor

--- student_week9_problem3.py
+++ student_week9_problem3.py
@@ -3,26 +3,28 @@ class Node:
         self.data = data
         self.next = None
 
+
 def print_linkedList(head):
     current = head
     while current:
         print(current.data, end=' ')
         current = current.next
-    print() 
+    print()
+
 
 class MergeLinkedList:
     def __init__(self):
-        '''=======Student's code======='''
+        """=======Student's code======="""
         self.head = None
-    
+
     def addList(self, head2):
-        '''=======Student's code======='''  
-        if head2 == None:
+        """=======Student's code======="""
+        if head2 is None:
             return
-        if self.head == None:
+        if self.head is None:
             self.head = head2
             return
-        
+
         if self.head.data <= head2.data:
             min_head = self.head
             cur = self.head.next
@@ -31,7 +33,7 @@ class MergeLinkedList:
             min_head = head2
             cur = self.head
             cur2 = head2.next
-        
+
         tmp_head = min_head
 
         while cur and cur2:
@@ -48,8 +50,9 @@ class MergeLinkedList:
             tmp_head.next = cur
         else:
             tmp_head.next = cur2
-        
+
         self.head = min_head
+
 
 def main():
     # Create two linked lists
@@ -77,6 +80,7 @@ def main():
     # Print the merged list
     print("Merged List: ", end='')
     print_linkedList(merged_list.head)
+
 
 if __name__ == '__main__':
     main()
