852bfdac744c4cb937917dcb30068af34cd28881
YYT\yyt <dbsxor0802@naver.com>
2025-05-08 23:36:12 +0900

    fix

--- student_week5_problem2.py
+++ student_week5_problem2.py
@@ -4,20 +4,17 @@ class Node:
         self.leftChild = None
         self.rightChild = None
 
-    def is_leaf(self):
-        return self.leftChild is None and self.rightChild is None
-
 
 max_sum = 0
 
 
 def maxPathSum(node):
     global max_sum
-    if node.is_leaf():
+    if node.rightChild is None and node.leftChild is None:
         return node.data
 
-    left_max = maxPathSum(node.leftChild)
-    right_max = maxPathSum(node.rightChild)
+    left_max = maxPathSum(node.leftChild) if node.leftChild else float('-inf')
+    right_max = maxPathSum(node.rightChild) if node.rightChild else float('-inf')
 
     if left_max + node.data + right_max > max_sum:
         max_sum = left_max + node.data + right_max
@@ -27,6 +24,7 @@ def maxPathSum(node):
 
 def findMaxPathSum(root):
     global max_sum
+    max_sum = float('-inf')
     maxPathSum(root)
     return max_sum
 
@@ -59,6 +57,34 @@ def main():
 
     # Output: 14 (8 + -2 + 1 + 3 + 4)
 
+    #           -10
+    #         /     \
+    #       7         8
+    #     /   \     /   \
+    #   -5     2   1     12
+    #  /  \              / \
+    # 3   -4           -2   6
+    #                         \
+    #                          9
+
+    root = Node(-10)
+    root.leftChild = Node(7)
+    root.rightChild = Node(8)
+
+    root.leftChild.leftChild = Node(-5)
+    root.leftChild.rightChild = Node(2)
+
+    root.rightChild.leftChild = Node(1)
+    root.rightChild.rightChild = Node(12)
+
+    root.leftChild.leftChild.leftChild = Node(3)
+    root.leftChild.leftChild.rightChild = Node(-4)
+
+    root.rightChild.rightChild.leftChild = Node(-2)
+    root.rightChild.rightChild.rightChild = Node(6)
+    root.rightChild.rightChild.rightChild.rightChild = Node(9)
+
+    print(findMaxPathSum(root)) # 36 (1 → 8 → 12 → 6 → 9)
 
 if __name__ == "__main__":
     main()
